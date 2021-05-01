d1026dda680e6b1c6eabacb2d59a304b95db8072
Aryan Gupta <aryan678gupta@gmail.com>
2021-05-01 18:25:23 +0530

    Third commit
    Updated map page and booking page

--- working.js
+++ working.js
@@ -1,8 +1,14 @@
 const mymail="johndoe@mail.com";
 const mypass="passpasspass";
-$('input').keypress(function(event){
+$('input#email, input#pass').keypress(function(event){
   if (event.keyCode === 13) {
     LoginPass();
+  }
+});
+
+$('input.location-input').keypress(function(event){
+  if (event.keyCode === 13) {
+    location.href="./BookingPage.html";
   }
 });
 function LoginPass(){
@@ -15,6 +21,18 @@ function LoginPass(){
     alert("Wrong username/password");
   }
 }
+
+function mapLocation(){
+  var locationName= $('.location-input').value();
+}
+
+$("input.license-number").on("click", function() {
+  $(this).select();
+});
+var todayDate = new Date().toISOString().slice(0, 10);
+$("input[type=date]").val(todayDate);
+
+$("input[type=time]").val(new Date().toLocaleTimeString());
 
 var licenseNo="";
 var bookingTime="";
