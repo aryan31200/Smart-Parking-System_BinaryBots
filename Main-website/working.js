63e1d3f9200d5dea600117f29d52ec4342588ca5
Aryan Gupta <aryan678gupta@gmail.com>
2021-05-13 15:46:35 +0530

    New commit
    Changed OCR software used and updated presentation

---
+++ Main-website/working.js
@@ -0,0 +1,56 @@
+const mymail = "johndoe@mail.com";
+const mypass = "passpasspass";
+$("input#email, input#pass").keypress(function (event) {
+  if (event.keyCode === 13) {
+    LoginPass();
+  }
+});
+
+$("input.location-input").keypress(function (event) {
+  if (event.keyCode === 13) {
+    location.href = "./BookingPage.html";
+  }
+});
+function LoginPass() {
+  var email = $("#email").val();
+  var pass = $("#pass").val();
+  console.log(email, pass);
+  if (email === mymail && pass === mypass) {
+    location.href = "./mapPage.html";
+  } else {
+    alert("Wrong username/password");
+  }
+}
+
+function mapLocation() {
+  var locationName = $(".location-input").value();
+}
+
+$("input.license-number").on("click", function () {
+  $(this).select();
+});
+var todayDate = new Date().toISOString().slice(0, 10);
+$("input[type=date]").val(todayDate);
+
+function timeChecker(){
+  if($("input[type=time]").val()==="14:30"){
+    $(".booked-spots").css("background-color","yellow");
+  }
+}
+
+var licenseNo = "";
+var bookingTime = "";
+function Book() {
+  location.href = "./Booked.html";
+}
+function ConfirmedEntry() {
+  location.href = "./pay.html";
+}
+function Payment() {
+  $(".loginbutton-payment").attr("disabled",false);
+  $(".loginbutton-payment").css("background-color", "#ff8533");
+}
+
+function Pay(){
+  location.href="./PaymentConfirmation.html"
+}
